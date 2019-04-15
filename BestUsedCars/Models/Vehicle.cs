4e5325db961b27e1363c33b2593a3b39c960b3da
Brett Peterson <pstlpete58@yahoo.com>
2019-04-15 10:36:36 -0700

    Created Vehicle.cs  with 8 attributes

---
+++ BestUsedCars/Models/Vehicle.cs
@@ -0,0 +1,23 @@
+﻿using BestUsedCars.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BestUsedCars.Models
+{
+    public class Vehicle
+    {
+        //I will use the Vehicle Vehicle Identification Number VIN as my Primary Key
+        //LotId is a self generated number for business use
+        public int LotID { get; set; }
+        public string VIN { get; set; }
+        public int Year { get; set; }
+        public string Make { get; set; }
+        public string Model { get; set; }
+        public double SalePrice { get; set; }
+        public string Color { get; set; }
+        public int Miles { get; set; }
+    }
+}
+
