555d5e3409718c4f45ac1c8d5decbcf9054a2276
Brett Peterson <pstlpete58@yahoo.com>
2019-04-23 20:39:50 -0700

    Added attributes to 6 fields including Key assigned to VIN number

--- BestUsedCars/Models/Vehicle.cs
+++ BestUsedCars/Models/Vehicle.cs
@@ -1,6 +1,7 @@
 ﻿using BestUsedCars.Models;
 using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -10,14 +11,27 @@ namespace BestUsedCars.Models
     {
         //I will use the Vehicle Vehicle Identification Number VIN as my Primary Key
         //LotId is a self generated number for business use
+       
         public int LotID { get; set; }
+        [Key]
+        [Required]
+        [StringLength (17)]
         public string VIN { get; set; }
+        [Required]
         public int Year { get; set; }
+        [Required]
+        [StringLength (30)]
         public string Make { get; set; }
+        [Required]
+        [StringLength (30)]
         public string Model { get; set; }
         public double SalePrice { get; set; }
+        [Required]
+        [StringLength(20)]
         public string Color { get; set; }
         public int Miles { get; set; }
+        [StringLength(20)]
+        public string PicId { get; set; }
     }
 }
 
