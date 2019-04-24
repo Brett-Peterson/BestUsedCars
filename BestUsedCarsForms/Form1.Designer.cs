cd14b509d2af500214a42b2eb76f9909b56b1b15
Brett Peterson <pstlpete58@yahoo.com>
2019-04-24 10:45:00 -0700

    Created a Forms Folder and created a Vehicle Purchase Form

---
+++ BestUsedCarsForms/Form1.Designer.cs
@@ -0,0 +1,356 @@
+﻿namespace BestUsedCarsForms
+{
+    partial class frmVehiclePurchase
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lblVIN = new System.Windows.Forms.Label();
+            this.tbVIN = new System.Windows.Forms.TextBox();
+            this.lblYear = new System.Windows.Forms.Label();
+            this.tbYear = new System.Windows.Forms.TextBox();
+            this.tbModel = new System.Windows.Forms.TextBox();
+            this.lblModel = new System.Windows.Forms.Label();
+            this.lblMake = new System.Windows.Forms.Label();
+            this.tbMake = new System.Windows.Forms.TextBox();
+            this.lblPrice = new System.Windows.Forms.Label();
+            this.tbPrice = new System.Windows.Forms.TextBox();
+            this.lblShipping = new System.Windows.Forms.Label();
+            this.tbShipping = new System.Windows.Forms.TextBox();
+            this.tbOtherFees = new System.Windows.Forms.TextBox();
+            this.lblOtherFees = new System.Windows.Forms.Label();
+            this.lblTotal = new System.Windows.Forms.Label();
+            this.tbTotal = new System.Windows.Forms.TextBox();
+            this.lblColor = new System.Windows.Forms.Label();
+            this.tbColor = new System.Windows.Forms.TextBox();
+            this.lblMiles = new System.Windows.Forms.Label();
+            this.tbMiles = new System.Windows.Forms.TextBox();
+            this.lblSeller = new System.Windows.Forms.Label();
+            this.tbSellName = new System.Windows.Forms.TextBox();
+            this.lblSellerID = new System.Windows.Forms.Label();
+            this.tbSellerID = new System.Windows.Forms.TextBox();
+            this.lblPurchaser = new System.Windows.Forms.Label();
+            this.tbPurchaser = new System.Windows.Forms.TextBox();
+            this.PurchaseDate = new System.Windows.Forms.MonthCalendar();
+            this.lblDate = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // lblVIN
+            // 
+            this.lblVIN.AutoSize = true;
+            this.lblVIN.Location = new System.Drawing.Point(45, 47);
+            this.lblVIN.Name = "lblVIN";
+            this.lblVIN.Size = new System.Drawing.Size(28, 13);
+            this.lblVIN.TabIndex = 0;
+            this.lblVIN.Text = "VIN:";
+            // 
+            // tbVIN
+            // 
+            this.tbVIN.Location = new System.Drawing.Point(89, 47);
+            this.tbVIN.Name = "tbVIN";
+            this.tbVIN.Size = new System.Drawing.Size(194, 20);
+            this.tbVIN.TabIndex = 1;
+            // 
+            // lblYear
+            // 
+            this.lblYear.AutoSize = true;
+            this.lblYear.Location = new System.Drawing.Point(48, 82);
+            this.lblYear.Name = "lblYear";
+            this.lblYear.Size = new System.Drawing.Size(32, 13);
+            this.lblYear.TabIndex = 2;
+            this.lblYear.Text = "Year:";
+            // 
+            // tbYear
+            // 
+            this.tbYear.Location = new System.Drawing.Point(89, 82);
+            this.tbYear.Name = "tbYear";
+            this.tbYear.Size = new System.Drawing.Size(56, 20);
+            this.tbYear.TabIndex = 3;
+            // 
+            // tbModel
+            // 
+            this.tbModel.Location = new System.Drawing.Point(89, 149);
+            this.tbModel.Name = "tbModel";
+            this.tbModel.Size = new System.Drawing.Size(100, 20);
+            this.tbModel.TabIndex = 4;
+            // 
+            // lblModel
+            // 
+            this.lblModel.AutoSize = true;
+            this.lblModel.Location = new System.Drawing.Point(41, 149);
+            this.lblModel.Name = "lblModel";
+            this.lblModel.Size = new System.Drawing.Size(39, 13);
+            this.lblModel.TabIndex = 5;
+            this.lblModel.Text = "Model:";
+            // 
+            // lblMake
+            // 
+            this.lblMake.AutoSize = true;
+            this.lblMake.Location = new System.Drawing.Point(43, 118);
+            this.lblMake.Name = "lblMake";
+            this.lblMake.Size = new System.Drawing.Size(37, 13);
+            this.lblMake.TabIndex = 6;
+            this.lblMake.Text = "Make:";
+            // 
+            // tbMake
+            // 
+            this.tbMake.Location = new System.Drawing.Point(89, 118);
+            this.tbMake.Name = "tbMake";
+            this.tbMake.Size = new System.Drawing.Size(100, 20);
+            this.tbMake.TabIndex = 7;
+            // 
+            // lblPrice
+            // 
+            this.lblPrice.AutoSize = true;
+            this.lblPrice.Location = new System.Drawing.Point(289, 54);
+            this.lblPrice.Name = "lblPrice";
+            this.lblPrice.Size = new System.Drawing.Size(82, 13);
+            this.lblPrice.TabIndex = 8;
+            this.lblPrice.Text = "Purchase Price:";
+            // 
+            // tbPrice
+            // 
+            this.tbPrice.Location = new System.Drawing.Point(374, 47);
+            this.tbPrice.Name = "tbPrice";
+            this.tbPrice.Size = new System.Drawing.Size(100, 20);
+            this.tbPrice.TabIndex = 9;
+            // 
+            // lblShipping
+            // 
+            this.lblShipping.AutoSize = true;
+            this.lblShipping.Location = new System.Drawing.Point(292, 88);
+            this.lblShipping.Name = "lblShipping";
+            this.lblShipping.Size = new System.Drawing.Size(74, 13);
+            this.lblShipping.TabIndex = 10;
+            this.lblShipping.Text = "Shipping Fees";
+            // 
+            // tbShipping
+            // 
+            this.tbShipping.Location = new System.Drawing.Point(388, 82);
+            this.tbShipping.Name = "tbShipping";
+            this.tbShipping.Size = new System.Drawing.Size(86, 20);
+            this.tbShipping.TabIndex = 11;
+            // 
+            // tbOtherFees
+            // 
+            this.tbOtherFees.Location = new System.Drawing.Point(388, 118);
+            this.tbOtherFees.Name = "tbOtherFees";
+            this.tbOtherFees.Size = new System.Drawing.Size(86, 20);
+            this.tbOtherFees.TabIndex = 12;
+            // 
+            // lblOtherFees
+            // 
+            this.lblOtherFees.AutoSize = true;
+            this.lblOtherFees.Location = new System.Drawing.Point(304, 124);
+            this.lblOtherFees.Name = "lblOtherFees";
+            this.lblOtherFees.Size = new System.Drawing.Size(62, 13);
+            this.lblOtherFees.TabIndex = 13;
+            this.lblOtherFees.Text = "Other Fees:";
+            // 
+            // lblTotal
+            // 
+            this.lblTotal.AutoSize = true;
+            this.lblTotal.Location = new System.Drawing.Point(295, 155);
+            this.lblTotal.Name = "lblTotal";
+            this.lblTotal.Size = new System.Drawing.Size(37, 13);
+            this.lblTotal.TabIndex = 14;
+            this.lblTotal.Text = "Total: ";
+            // 
+            // tbTotal
+            // 
+            this.tbTotal.Location = new System.Drawing.Point(374, 155);
+            this.tbTotal.Name = "tbTotal";
+            this.tbTotal.Size = new System.Drawing.Size(100, 20);
+            this.tbTotal.TabIndex = 15;
+            // 
+            // lblColor
+            // 
+            this.lblColor.AutoSize = true;
+            this.lblColor.Location = new System.Drawing.Point(44, 185);
+            this.lblColor.Name = "lblColor";
+            this.lblColor.Size = new System.Drawing.Size(34, 13);
+            this.lblColor.TabIndex = 16;
+            this.lblColor.Text = "Color:";
+            // 
+            // tbColor
+            // 
+            this.tbColor.Location = new System.Drawing.Point(89, 185);
+            this.tbColor.Name = "tbColor";
+            this.tbColor.Size = new System.Drawing.Size(100, 20);
+            this.tbColor.TabIndex = 17;
+            // 
+            // lblMiles
+            // 
+            this.lblMiles.AutoSize = true;
+            this.lblMiles.Location = new System.Drawing.Point(44, 228);
+            this.lblMiles.Name = "lblMiles";
+            this.lblMiles.Size = new System.Drawing.Size(34, 13);
+            this.lblMiles.TabIndex = 18;
+            this.lblMiles.Text = "Miles:";
+            // 
+            // tbMiles
+            // 
+            this.tbMiles.Location = new System.Drawing.Point(89, 220);
+            this.tbMiles.Name = "tbMiles";
+            this.tbMiles.Size = new System.Drawing.Size(100, 20);
+            this.tbMiles.TabIndex = 19;
+            // 
+            // lblSeller
+            // 
+            this.lblSeller.AutoSize = true;
+            this.lblSeller.Location = new System.Drawing.Point(12, 300);
+            this.lblSeller.Name = "lblSeller";
+            this.lblSeller.Size = new System.Drawing.Size(69, 13);
+            this.lblSeller.TabIndex = 20;
+            this.lblSeller.Text = "Sellers Name";
+            // 
+            // tbSellName
+            // 
+            this.tbSellName.Location = new System.Drawing.Point(89, 297);
+            this.tbSellName.Name = "tbSellName";
+            this.tbSellName.Size = new System.Drawing.Size(183, 20);
+            this.tbSellName.TabIndex = 21;
+            // 
+            // lblSellerID
+            // 
+            this.lblSellerID.AutoSize = true;
+            this.lblSellerID.Location = new System.Drawing.Point(12, 332);
+            this.lblSellerID.Name = "lblSellerID";
+            this.lblSellerID.Size = new System.Drawing.Size(47, 13);
+            this.lblSellerID.TabIndex = 22;
+            this.lblSellerID.Text = "Seller ID";
+            // 
+            // tbSellerID
+            // 
+            this.tbSellerID.Location = new System.Drawing.Point(89, 329);
+            this.tbSellerID.Name = "tbSellerID";
+            this.tbSellerID.Size = new System.Drawing.Size(183, 20);
+            this.tbSellerID.TabIndex = 23;
+            // 
+            // lblPurchaser
+            // 
+            this.lblPurchaser.AutoSize = true;
+            this.lblPurchaser.Location = new System.Drawing.Point(12, 367);
+            this.lblPurchaser.Name = "lblPurchaser";
+            this.lblPurchaser.Size = new System.Drawing.Size(83, 13);
+            this.lblPurchaser.TabIndex = 24;
+            this.lblPurchaser.Text = "Purchase Agent";
+            // 
+            // tbPurchaser
+            // 
+            this.tbPurchaser.Location = new System.Drawing.Point(101, 360);
+            this.tbPurchaser.Name = "tbPurchaser";
+            this.tbPurchaser.Size = new System.Drawing.Size(183, 20);
+            this.tbPurchaser.TabIndex = 25;
+            // 
+            // PurchaseDate
+            // 
+            this.PurchaseDate.Location = new System.Drawing.Point(333, 280);
+            this.PurchaseDate.Name = "PurchaseDate";
+            this.PurchaseDate.TabIndex = 26;
+            // 
+            // lblDate
+            // 
+            this.lblDate.AutoSize = true;
+            this.lblDate.Location = new System.Drawing.Point(424, 255);
+            this.lblDate.Name = "lblDate";
+            this.lblDate.Size = new System.Drawing.Size(78, 13);
+            this.lblDate.TabIndex = 27;
+            this.lblDate.Text = "Purchase Date";
+            // 
+            // frmVehiclePurchase
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(569, 450);
+            this.Controls.Add(this.lblDate);
+            this.Controls.Add(this.PurchaseDate);
+            this.Controls.Add(this.tbPurchaser);
+            this.Controls.Add(this.lblPurchaser);
+            this.Controls.Add(this.tbSellerID);
+            this.Controls.Add(this.lblSellerID);
+            this.Controls.Add(this.tbSellName);
+            this.Controls.Add(this.lblSeller);
+            this.Controls.Add(this.tbMiles);
+            this.Controls.Add(this.lblMiles);
+            this.Controls.Add(this.tbColor);
+            this.Controls.Add(this.lblColor);
+            this.Controls.Add(this.tbTotal);
+            this.Controls.Add(this.lblTotal);
+            this.Controls.Add(this.lblOtherFees);
+            this.Controls.Add(this.tbOtherFees);
+            this.Controls.Add(this.tbShipping);
+            this.Controls.Add(this.lblShipping);
+            this.Controls.Add(this.tbPrice);
+            this.Controls.Add(this.lblPrice);
+            this.Controls.Add(this.tbMake);
+            this.Controls.Add(this.lblMake);
+            this.Controls.Add(this.lblModel);
+            this.Controls.Add(this.tbModel);
+            this.Controls.Add(this.tbYear);
+            this.Controls.Add(this.lblYear);
+            this.Controls.Add(this.tbVIN);
+            this.Controls.Add(this.lblVIN);
+            this.Name = "frmVehiclePurchase";
+            this.Text = "Vehicle Purchase Form";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblVIN;
+        private System.Windows.Forms.TextBox tbVIN;
+        private System.Windows.Forms.Label lblYear;
+        private System.Windows.Forms.TextBox tbYear;
+        private System.Windows.Forms.TextBox tbModel;
+        private System.Windows.Forms.Label lblModel;
+        private System.Windows.Forms.Label lblMake;
+        private System.Windows.Forms.TextBox tbMake;
+        private System.Windows.Forms.Label lblPrice;
+        private System.Windows.Forms.TextBox tbPrice;
+        private System.Windows.Forms.Label lblShipping;
+        private System.Windows.Forms.TextBox tbShipping;
+        private System.Windows.Forms.TextBox tbOtherFees;
+        private System.Windows.Forms.Label lblOtherFees;
+        private System.Windows.Forms.Label lblTotal;
+        private System.Windows.Forms.TextBox tbTotal;
+        private System.Windows.Forms.Label lblColor;
+        private System.Windows.Forms.TextBox tbColor;
+        private System.Windows.Forms.Label lblMiles;
+        private System.Windows.Forms.TextBox tbMiles;
+        private System.Windows.Forms.Label lblSeller;
+        private System.Windows.Forms.TextBox tbSellName;
+        private System.Windows.Forms.Label lblSellerID;
+        private System.Windows.Forms.TextBox tbSellerID;
+        private System.Windows.Forms.Label lblPurchaser;
+        private System.Windows.Forms.TextBox tbPurchaser;
+        private System.Windows.Forms.MonthCalendar PurchaseDate;
+        private System.Windows.Forms.Label lblDate;
+    }
+}
+
